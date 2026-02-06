25d84090a6eaad0f56347f250d8f4ac941610f1e
XZYW <928144597@qq.com>
2026-02-06 22:17:23 +0800

    Lesson 05-1 Textures Load

---
+++ VibeDX12Renderer/Lessons/06_Textures_Load/xmake.lua
@@ -0,0 +1,11 @@
+target("06_Textures_Load")
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_deps("VibeCommon")
+    add_syslinks("user32", "gdi32", "d3d12", "dxgi", "d3dcompiler")
+    
+    after_build(function (target)
+        import("core.project.task")
+        os.cp("$(scriptdir)/src/shaders.hlsl", target:targetdir())
+        os.cp("$(projectdir)/Assets", target:targetdir())
+    end)
